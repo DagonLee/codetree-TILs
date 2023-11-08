ffb65b9255442e3fb7ff46a27382d141a4e3ce98
DagonLee <43575986+DagonLee@users.noreply.github.com>
2023-11-08 11:27:07 +0900

    Title: 두 수의 합, Time: 479ms, Memory: 27MB, Status: Passed - CodeTree

--- 231108/두 수의 합/sum-of-two-num.java
+++ 231108/두 수의 합/sum-of-two-num.java
@@ -10,19 +10,26 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
         int[] nums = new int[n];
-        int cnt = 0;
+        int sum = 0;
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++){
             int val = Integer.parseInt(st.nextToken());
             nums[i] = val;
             map.put(val, map.getOrDefault(val, 0) + 1);
         }
-        for(int i = 0; i < n; i++){
-            int target = k - nums[i];
+        Set<Integer>keySet = map.keySet();
+        for(int key: keySet){
+            int target = k - key;
             if(map.containsKey(target)){
-                cnt+= map.get(nums[i]) * map.get(target);
+                if(target == key){
+                    int cnt = map.get(target);
+                    sum += ((cnt * (cnt - 1)));
+                }
+                else{
+                    sum += (map.get(key) * map.get(target));
+                }
             }
         }
-        System.out.println(cnt / 2);
+        System.out.println(sum / 2);
     }
 }
