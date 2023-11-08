39892d140889b631bd1126569ec75c58cedda063
DagonLee <43575986+DagonLee@users.noreply.github.com>
2023-11-08 11:14:29 +0900

    Title: 두 수의 합, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231108/두 수의 합/sum-of-two-num.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, k;
+    static HashMap<Integer, Integer> map = new HashMap<>();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        int[] nums = new int[n];
+        int cnt = 0;
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            int val = Integer.parseInt(st.nextToken());
+            nums[i] = val;
+            map.put(val, map.getOrDefault(val, 0) + 1);
+        }
+        for(int i = 0; i < n; i++){
+            int target = k - nums[i];
+            if(map.containsKey(target)){
+                cnt+= map.get(target);
+            }
+        }
+        System.out.println(cnt / 2);
+    }
+}
