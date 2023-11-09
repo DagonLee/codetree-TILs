564b0ed744da1a95f9f106b3200bf46aec17259b
DagonLee <43575986+DagonLee@users.noreply.github.com>
2023-11-09 13:14:37 +0900

    Title: 비율 구하기, Time: 532ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 231109/비율 구하기/find-proportions.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static double n;
+
+    public static void main(String[] args) throws IOException{
+        TreeMap<String, Double> map = new TreeMap<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Double.parseDouble(br.readLine());
+        for(int i = 0; i < n; i++){
+            String str = br.readLine();
+            map.put(str, map.getOrDefault(str, 0.0000) + 1);
+
+        }
+        Iterator<Map.Entry<String, Double>> it = map.entrySet().iterator();
+        while(it.hasNext()){
+            Map.Entry<String, Double> entry = it.next();
+            System.out.println(entry.getKey() + " " + String.format("%.4f",(double)Math.round((entry.getValue() / n) * 10000) / 100));
+        }
+        
+    }
+}
