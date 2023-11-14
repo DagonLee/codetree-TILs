7b9383598035673bbbd5b29a049403463bd78e98
DagonLee <43575986+DagonLee@users.noreply.github.com>
2023-11-14 17:32:04 +0900

    Title: 선분 위의 점, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231114/선분 위의 점/point-on-the-line-segment.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] arr;
+    static int upperBound(int target){
+        int left = 0;
+        int right = n - 1;
+        int idx = n;
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(arr[mid] > target){
+                right = mid - 1;
+                idx = Math.min(idx, mid);
+            }else{
+                left = mid + 1;
+            }
+        }
+        return idx;
+    }
+    static int lowerBound(int target){
+        int left = 0;
+        int right = n - 1;
+        int idx = n;
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(arr[mid] > target){
+                right = mid - 1;
+                idx = Math.min(idx, mid);
+            }else{
+                left = mid + 1;
+            }
+        }
+        return idx;
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(upperBound(b) - lowerBound(a));
+        }
+    }
+}
