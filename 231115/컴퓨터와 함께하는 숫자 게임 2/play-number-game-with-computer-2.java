fd68149f34c028941b629de00ddb204d0af7917b
DagonLee <43575986+DagonLee@users.noreply.github.com>
2023-11-15 10:11:57 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231115/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.java
+++ 231115/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.java
@@ -2,15 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int m;
-    public static int search(int target){
-        int left = 1;
-        int right = m;
-        int cnt = 1;
+    static long m;
+    public static long search(long target){
+        long left = 1;
+        long right = m;
+        long cnt = 0;
         while(left <= right){
-            int mid = (left + right) / 2;
+            cnt++;
+            long mid = (left + right) / 2;
             if(target == mid){
-                break;
+                return cnt;
             }
             else if(target < mid){
                 right = mid - 1;
@@ -18,20 +19,20 @@ public class Main {
             else{
                 left = mid + 1;
             }
-            cnt++;
+            
         }
-        return cnt;
+        return -1;
     }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         m = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int min = m;
-        int max = 0;
-        for(int i = a; i <= b; i++){
-            int res = search(i);
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
+        long min = 1000000000;
+        long max = 0;
+        for(long i = a; i <= b; i++){
+            long res = search(i);
             min = Math.min(res, min);
             max = Math.max(res, max);
         }
